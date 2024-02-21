1dcfe92aeff4f2d52d977402b131614698c3b43f
Rosario <marcodibartolo2@gmail.com>
2024-02-21 16:41:53 +0100

    Adding initial files

---
+++ Info.py
@@ -0,0 +1,5 @@
+Nome = "WorkerAnalyzer"
+Descrizione = f"""
+"{Nome}" è un software designato per l'automatizzazzione di diversi processi relativi all'ambito gestionale di lavoratori/dipendenti do un azienda, o semplici componenti di istituzioni o associazioni indipendentemente dallo scopo =)
+"""
+
